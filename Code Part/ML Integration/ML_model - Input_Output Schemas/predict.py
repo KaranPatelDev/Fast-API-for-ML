8e980ca8bc124631307ba07ad0c2de291c3037ae
Karantechie <karankpguinfo001018@gmail.com>
2025-08-30 11:23:42 +0530

    Refactor prediction script to include model loading and prediction function

--- Code Part/ML Integration/ML_model - Input_Output Schemas/predict.py
+++ Code Part/ML Integration/ML_model - Input_Output Schemas/predict.py
@@ -1,1 +1,24 @@
 # Contains all the code for making predictions using the trained model
+
+import joblib
+import numpy as np
+
+# Load the trained model from the file
+saved_model = joblib.load('model.joblib')
+print("Loaded the model from model.joblib")
+
+def make_prediction(data: dict) -> float:
+    features = np.array([
+        [
+            data['longitude'],
+            data['latitude'],
+            data['housing_median_age'],
+            data['total_rooms'],
+            data['total_bedrooms'],
+            data['population'],
+            data['households'],
+            data['median_income']
+        ]
+    ])
+    return saved_model.predict(features)[0]
+
