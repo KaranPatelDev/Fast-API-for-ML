3042867ffa0f0b7c6171cdeb659bacb64393b424
Karantechie <karankpguinfo001018@gmail.com>
2025-09-01 15:12:49 +0530

    Implement user authentication endpoints with token generation and validation

--- Code Part/JWT Authentication/auth.py
+++ Code Part/JWT Authentication/auth.py
@@ -1,32 +1,29 @@
 from datetime import datetime, timedelta, timezone
-from authlib.jose import jwt, JWTError
+from authlib.jose import JoseError, jwt
 from fastapi import HTTPException
 
+# constants
+SECRET_KEY = 'my_secret'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRY_MINUTES = 30
 
-# Constants
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-
-# Function to create a JWT token
+# functions
 def create_access_token(data: dict):
-    header = {"alg": ALGORITHM}
-    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    header = {'alg': ALGORITHM}
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRY_MINUTES)
     payload = data.copy()
-    payload.update({"exp": expire})
-    return jwt.encode(header, payload, SECRET_KEY).decode("utf-8")
+    payload.update({'exp': expire})
+    return jwt.encode(header, payload, SECRET_KEY).decode('utf-8')
 
 
-# Function to verify a JWT token
 def verify_token(token: str):
     try:
         claims = jwt.decode(token, SECRET_KEY)
-        claims.validate()  # This will check the 'exp' claim
-        username = claims.get("sub")
+        claims.validate()
+        username = claims.get('sub')
         if username is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(status_code=401, detail='Token missing')
         return username
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
-
+    except JoseError:
+        raise HTTPException(status_code=401, detail="Couldn't Validate Credentials")
